e42d52274ab9e5445df21b12137f9c6a0fff22c7
Tasha Van Dayne <vandat@sunypoly.edu>
2025-10-29 19:36:56 -0400

    added main and other files

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <vector>
+#include <cstdio>
+#include "../teenyat.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    
+}
